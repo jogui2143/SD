4622566417c3d921dcce0d4572aa16e66085cb92
jogui2143 <jogui2143@hotmail.com.com>
2024-05-09 20:44:43 +0100

    caralho

---
+++ meta2/demo/src/main/java/joao_rodrigo/demo/SdMt2Application.java
@@ -0,0 +1,13 @@
+package joao_rodrigo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdMt2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdMt2Application.class, args);
+	}
+
+}
---
+++ meta2/demo/src/test/java/joao_rodrigo/demo/SdMt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package joao_rodrigo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdMt2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
